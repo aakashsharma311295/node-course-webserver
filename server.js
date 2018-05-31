d58e857f21ce2c1da1d5cf91a8f469eec53f66f6
aakash <aakashsharma311295@gmail.com>
2018-06-01 01:31:12 +0530

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const hbs = require("hbs");
 const fs = require("fs");
+const port = process.env.PORT||3000;
 
 
 
@@ -57,4 +58,4 @@ app.get("/bad", (req, res) => {
 
 
 
-app.listen(3000, () => console.log("server is up on 3000 port"));
+app.listen(port, () => console.log(`server is up on ${port} port`));
